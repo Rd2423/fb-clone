4a82661b01ec9471c838267e913583b91682fffd
Rayan Diouf <rayandiouf@Rayans-MacBook-Pro.local>
2021-11-23 15:24:21 -0500

    header component

---
+++ src/firebase.js
@@ -0,0 +1,10 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+    apiKey: "AIzaSyD37JnSgfV0YN8aQRyhb89vGJSwJOVLgks",
+    authDomain: "facebook-clone-65b8b.firebaseapp.com",
+    projectId: "facebook-clone-65b8b",
+    storageBucket: "facebook-clone-65b8b.appspot.com",
+    messagingSenderId: "769928494333",
+    appId: "1:769928494333:web:9c02abc3e6379a4e959294",
+    measurementId: "G-YLYEG84RY4"
+  };
