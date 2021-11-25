345629de648b70be699ac11adebd951d0501d900
Rayan Diouf <rayandiouf@Rayans-MacBook-Pro.local>
2021-11-25 15:26:30 -0500

    working on making the workingSender component work with the Post component

--- src/MessageSender.js
+++ src/MessageSender.js
@@ -1,47 +1,58 @@
-import React from 'react'
-import {Avatar} from "@material-ui/core"
-import './MessageSender.css'
-import VideocamIcon from '@mui/icons-material/Videocam';
-import PhotoLibraryIcon from '@mui/icons-material/PhotoLibrary';
-import InsertEmoticonIcon from '@mui/icons-material/InsertEmoticon';
+import React from "react";
+import { Avatar } from "@material-ui/core";
+import "./MessageSender.css";
+import { useState } from "react";
+import VideocamIcon from "@mui/icons-material/Videocam";
+import PhotoLibraryIcon from "@mui/icons-material/PhotoLibrary";
+import InsertEmoticonIcon from "@mui/icons-material/InsertEmoticon";
 
 function MessageSender() {
-    const handleSubmit = e => {
-        e.preventDefault();
-    }
-    return (
-        <div className="messageSender">
-            <div className="messageSender_top">
-                <Avatar />
-                <form>
-                    <input type="text" placeholder={`What's on your mind`}/>
-                    <input placeholder="image URL (Optional" />
-                    <button onClick={handleSubmit} type="submit">
-                        Hidden Submit
-                    </button>
+  const [input, setInput] = useState("");
+  const [imageURL, setImageURL] = useState("");
+  const handleSubmit = (e) => {
+    e.preventDefault();
 
-                </form>
-            </div>
+    setInput('')
+    setImageURL('')
+  };
+  return (
+    <div className="messageSender">
+      <div className="messageSender_top">
+        <Avatar />
+        <form>
+          <input
+            value={input}
+            onChange={(e) => setInput(e.target.value)}
+            className="messageSender_input"
+            placeholder={`What's on your mind`}
+          />
+          <input
+            value={imageURL}
+            onChange={(e) => setImageURL(e.target.value)}
+            placeholder="image URL (Optional"
+          />
+          <button onClick={handleSubmit} type="submit">
+            Hidden Submit
+          </button>
+        </form>
+      </div>
 
-            <div className="messageSender_bottom">
-                <div className="messageSender_option">
-
-                <VideocamIcon style={{color: "red"}}/>
-                <h3>Live Video</h3>
-                </div>
-                <div className="messageSender_option">
-
-                <PhotoLibraryIcon style={{color: "green"}}/>
-                <h3>Photo/Video</h3>
-                </div>
-                <div className="messageSender_option">
-
-                <InsertEmoticonIcon style={{color: "lightblue"}}/>
-                <h3>Feeling/Activity</h3>
-                </div>
-            </div>
+      <div className="messageSender_bottom">
+        <div className="messageSender_option">
+          <VideocamIcon style={{ color: "red" }} />
+          <h3>Live Video</h3>
         </div>
-    )
+        <div className="messageSender_option">
+          <PhotoLibraryIcon style={{ color: "green" }} />
+          <h3>Photo/Video</h3>
+        </div>
+        <div className="messageSender_option">
+          <InsertEmoticonIcon style={{ color: "lightblue" }} />
+          <h3>Feeling/Activity</h3>
+        </div>
+      </div>
+    </div>
+  );
 }
 
-export default MessageSender
+export default MessageSender;
